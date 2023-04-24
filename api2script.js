ef1e09c8253c866646c4150ec52a29f33e15d5f8
SudharsonKR <123262424+SudharsonKR@users.noreply.github.com>
2023-04-24 21:31:25 +0530

    Update api2script.js

--- api2script.js
+++ api2script.js
@@ -1,17 +1,30 @@
 async function api2fetchdata(){
     try{
-    const response= await fetch("https://api-thirukkural.vercel.app/api?num=10")
-    const data = await response.json()
-    console.log(response)
-    console.log(data)
+    const response1= await fetch("https://api-thirukkural.vercel.app/api?num=1")
+    const data1 = await response1.json()
+    const response2= await fetch("https://api-thirukkural.vercel.app/api?num=2")
+    const data2 = await response2.json()
+    const response3= await fetch("https://api-thirukkural.vercel.app/api?num=3")
+    const data3 = await response3.json()
+    const response4= await fetch("https://api-thirukkural.vercel.app/api?num=4")
+    const data4 = await response4.json()
+    const response5= await fetch("https://api-thirukkural.vercel.app/api?num=5")
+    const data5 = await response5.json()
+    const response10= await fetch("https://api-thirukkural.vercel.app/api?num=10")
+    const data10 = await response10.json()
+    const arrays =[data1, data2, data3, data4, data5, data10]
+    console.log(response10)
+    console.log(data10)
+    console.log(arrays)
+    arrays.forEach(ele=>{
         const kural={
             
-            Firstline : data.line1,
-            Secondline : data.line2,
-            Explanation : data.tam_exp
+            Firstline : ele.line1,
+            Secondline : ele.line2,
+            Explanation : ele.tam_exp
         }
         Thirukural(kural)
-        
+    })
     }
     catch (error)
     {
